13ceff7266c951306c7599e6dbc94d838bd6cc47
Justin Damon <justindamon84@gmail.com>
2021-08-28 11:56:13 -0400

    add

---
+++ public/scripts/index.js
@@ -0,0 +1,65 @@
+const tipForm = document.getElementById('tip-form');
+const tipsContainer = document.getElementById('tip-container');
+
+const createCard = (tip) => {
+  // Create card
+  const cardEl = document.createElement('div');
+  cardEl.classList.add('card', 'mb-3');
+  cardEl.setAttribute('key', tip.tip_id);
+
+  // Create card header
+  const cardHeaderEl = document.createElement('h4');
+  cardHeaderEl.classList.add(
+    'card-header',
+    'bg-primary',
+    'text-light',
+    'p-2',
+    'm-0'
+  );
+  cardHeaderEl.innerHTML = `${tip.username} </br>`;
+
+  // Create card body
+  const cardBodyEl = document.createElement('div');
+  cardBodyEl.classList.add('card-body', 'bg-light', 'p-2');
+  cardBodyEl.innerHTML = `<p>${tip.tip}</p>`;
+
+  // Append the header and body to the card element
+  cardEl.appendChild(cardHeaderEl);
+  cardEl.appendChild(cardBodyEl);
+
+  // Append the card element to the tips container in the DOM
+  tipsContainer.appendChild(cardEl);
+};
+
+// Get a list of existing tips from the server
+const getTips = () =>
+  fetch('/api/tips', {
+    method: 'GET', // or 'PUT'
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    // body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => data)
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+
+// Post a new tip to the page
+const postTip = (tip) =>
+  fetch('/api/tips', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(tip),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      alert(data);
+      createCard(tip);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
